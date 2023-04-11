a08e74fced1e08bbd5b2aa0ce6acae29c06e97ac
amy_pr <ampreci@gmail.com>
2023-04-10 17:42:38 -0700

    inputs

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -5,6 +5,18 @@ export function AddItem() {
 				Item name:
 				<input type="text" id="item-name-input" placeholder="eggs"></input>
 			</label>
+
+			<fieldset>
+				<p>How soon will you buy this again?</p>
+				<input type="radio" id="soon" name="time" />
+				<label htmlFor="soon">Soon</label> <br />
+				<input type="radio" id="kind-of-soon" name="time" />
+				<label htmlFor="kind-of-soon">Kind of soon</label> <br />
+				<input type="radio" id="not-soon" name="time" />
+				<label htmlFor="not-soon">Not soon</label>
+			</fieldset>
+
+			<input type="submit" id="Add Item" />
 		</form>
 	);
 }
