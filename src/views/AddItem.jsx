115aeedca49de369d7cc6326df3260c7fff285d7
Emily Sellers <emilymelindasellers@gmail.com>
2023-04-10 17:42:05 -0700

    add radio button input for soon option

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 export function AddItem() {
 	return (
 		<form>
 			<label for="item-name-input">
 				Item name:
 				<input type="text" id="item-name-input" placeholder="eggs"></input>
+			</label>
+			<p>How soon will you buy this again?</p>
+			<label>
+				<input type="radio"></input>Soon
 			</label>
 		</form>
 	);
