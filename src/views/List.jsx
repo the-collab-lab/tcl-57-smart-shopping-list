d95eaccbc0196cb9159fd9b95cf14cbeda43e3ef
Beth <bethmelmtv@gmail.com>
2023-04-17 18:17:52 -0700

    added state andsetup event listener

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [filter, setFilter] = useState('');
+
 	return (
 		<>
 			<form>
 				<label htmlFor="search-filter">Item name:</label>
-				<input type="text" id="search-filter" />
+				<input
+					type="text"
+					id="search-filter"
+					onChange={(e) => setFilter(e.target.value)}
+				/>
 			</form>
 			<ul>
 				{data.map((item) => (
