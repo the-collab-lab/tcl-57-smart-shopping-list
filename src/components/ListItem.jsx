d75d8ebb92aaa89ec4867a8d7d6fa6428b3ee106
amy_pr <ampreci@gmail.com>
2023-04-24 17:59:13 -0700

    set up event listener in list item checkbox

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,15 @@
 import './ListItem.css';
 
-export function ListItem({ name, itemId }) {
+// Passing to each list item the setter function "setCheckedItem"
+
+export function ListItem({ name, itemId, setCheckedItemId }) {
 	return (
 		<li className="ListItem">
-			<input type="checkbox" id={itemId} />
+			<input
+				type="checkbox"
+				id={itemId}
+				onChange={() => setCheckedItemId(itemId)}
+			/>
 			<label htmlFor={itemId}>{name}</label>
 		</li>
 	);
