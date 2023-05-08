6b63849cd8d56b5141affe807135d8ccd73f400a
amy_pr <ampreci@gmail.com>
2023-05-08 16:39:18 -0700

    complete pseudocode steps in ListItem component

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -7,6 +7,21 @@ export function ListItem({
 	setIsChecked,
 	setCheckedItemId,
 }) {
+	//1. Add a button inside the <li> element for each list item.
+	//<button type="button">Delete</button>
+
+	//2. OnClick of the button call an event handler
+	//<button type="button" onClick={deleteItemFromList}>Delete</button>
+
+	//3. The event handler a) will alert user with prompt, b) call the deleteItem function from firebase
+
+	//function deleteItemFromList {
+	//if (window.confirm("Do you really want to delete this item?")) {
+	//window.open("Your item has been deleted");
+	//}
+
+	//}
+
 	function clickHandler(event, itemId) {
 		setIsChecked(event.target.checked);
 		setCheckedItemId(itemId);
