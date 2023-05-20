b7aae7b6b2f171beae424698d88d14e8e37407ea
amy_pr <ampreci@gmail.com>
2023-05-19 21:00:36 -0700

    define containers for header, nav and main areas of layout

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,29 +1,52 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import { Container } from '@mui/material';
+import { Typography } from '@mui/material';
+import { Paper } from '@mui/material';
 
 // import './Layout.css';
 
 export function Layout() {
 	return (
 		<>
-			<div className="Layout">
-				<header className="Layout-header">
-					<h1>Smart shopping list</h1>
-				</header>
-				<main className="Layout-main">
-					<Outlet />
-				</main>
-				<nav className="Nav">
-					<NavLink to="/" className="Nav-link">
-						Home
-					</NavLink>
-					<NavLink to="/list" className="Nav-link">
-						List
-					</NavLink>
-					<NavLink to="/add-item" className="Nav-link">
-						Add Item
-					</NavLink>
-				</nav>
-			</div>
+			{/*Possibly replace this with AppBar component*/}
+			<Container maxWidth="false" sx={{ bgcolor: 'silver' }}>
+				<Container
+					maxWidth="false"
+					sx={{ bgcolor: '#cfe8fc', display: 'flex', justifyContent: 'center' }}
+				>
+					<header className="Layout-header">
+						<nav className="Nav">
+							<NavLink to="/" className="Nav-link">
+								Home
+							</NavLink>
+							<NavLink to="/list" className="Nav-link">
+								List
+							</NavLink>
+							<NavLink to="/add-item" className="Nav-link">
+								Add Item
+							</NavLink>
+						</nav>
+						<Typography variant="h3" gutterBottom>
+							Smart shopping list
+						</Typography>
+					</header>
+				</Container>
+				{/* <Paper> */}
+				<Container
+					maxWidth="false"
+					sx={{
+						bgcolor: 'lightpink',
+						display: 'flex',
+						justifyContent: 'center',
+						height: '100vh',
+					}}
+				>
+					<main className="Layout-main">
+						<Outlet />
+					</main>
+				</Container>
+				{/* </Paper> */}
+			</Container>
 		</>
 	);
 }
