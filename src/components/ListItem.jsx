38ad2fc7e2d0cbfe27f20fa152e56cede660391a
Jon Granados <grxnxdos@gmail.com>
2023-05-21 14:50:28 -0700

    feat: replace <input> and <label> with MUI ListItemButton, ListItemIcon, Checkbox, and ListItemText to handle purchasing functionality

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,8 +1,21 @@
 import './ListItem.css';
 
-import { ListItem } from '@mui/material';
+import {
+	ListItem,
+	ListItemButton,
+	ListItemText,
+	ListItemIcon,
+	Checkbox,
+} from '@mui/material';
 
-export function ListItemComponent({ name, urgency }) {
+export function ListItemComponent({
+	name,
+	itemId,
+	urgency,
+	isDefaultChecked,
+	setIsChecked,
+	setCheckedItemId,
+}) {
 	const iconsByUrgency = {
 		soon: '🟠',
 		'kind of soon': '🟡',
@@ -13,7 +26,33 @@ export function ListItemComponent({ name, urgency }) {
 
 	const urgencyIcon = iconsByUrgency[urgency];
 
+	function clickHandler(event, itemId) {
+		setIsChecked(event.target.checked);
+		setCheckedItemId(itemId);
+	}
+
 	return (
-		<ListItem disablePadding>{`${name} (${urgencyIcon} ${urgency})`}</ListItem>
+		<ListItem disablePadding>
+			<ListItemButton
+				role={undefined}
+				onClick={(event) => clickHandler(event, itemId)}
+				dense
+			>
+				<ListItemIcon>
+					<Checkbox
+						id={itemId}
+						edge="start"
+						defaultChecked={isDefaultChecked}
+						tabIndex={-1}
+						disableRipple
+						inputProps={{ 'aria-labelledby': `checkbox-liist-label=${name}` }}
+					/>
+				</ListItemIcon>
+				<ListItemText
+					id={itemId}
+					primary={`${name} (${urgencyIcon} ${urgency})`}
+				/>
+			</ListItemButton>
+		</ListItem>
 	);
 }
