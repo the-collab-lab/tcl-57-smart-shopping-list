edce19e743d97060a8f3bb27d87008b7c1968e9f
amy_pr <ampreci@gmail.com>
2023-05-21 14:51:13 -0700

    replace links with nav links and style headline for full screen view

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -13,6 +13,7 @@ import {
 	Button,
 	IconButton,
 } from '@mui/material';
+// import { flexbox } from '@mui/system'
 // import MenuIcon from '@mui/icons-material/Menu';
 // import './Layout.css';
 
@@ -21,53 +22,88 @@ import {
 export function Layout() {
 	return (
 		<>
-			<Container maxWidth="false" sx={{ bgcolor: '#739C83' }}>
-				<AppBar position="relative">
-					<Container maxWidth="xl">
-						<Toolbar>
-							<Typography
-								variant="h6"
-								noWrap
-								component="a"
-								href="/"
-								sx={{
-									mr: 2,
-									display: { xs: 'none', md: 'flex' },
-									fontFamily: 'monospace',
-									fontWeight: 700,
-									letterSpacing: '.3rem',
-									color: 'inherit',
-									textDecoration: 'none',
-								}}
-							>
-								Smart Shopping List
-							</Typography>
+			<Container
+				maxWidth="false"
+				sx={{ bgcolor: '#739C83', display: 'flex', flexDirection: 'column' }}
+			>
+				<Container maxWidth="lg" sx={{ p: '0' }}>
+					<AppBar position="relative">
+						<Container maxWidth="xl">
+							<Toolbar>
+								<Typography
+									variant="h5"
+									noWrap
+									component="a"
+									href="/"
+									sx={{
+										mr: 2,
+										display: { xs: 'none', md: 'flex' },
+										fontFamily: 'monospace',
+										fontWeight: 700,
+										letterSpacing: '.3rem',
+										color: 'inherit',
+										textDecoration: 'none',
+									}}
+								>
+									Smart Shopping List
+								</Typography>
 
-							<div>
-								<Breadcrumbs aria-label="breadcrumb">
-									<Link underline="hover" color="inherit" href="/">
+								<div>
+									<Breadcrumbs aria-label="breadcrumb">
+										<NavLink to="/" className="Nav-link">
+											Home
+										</NavLink>
+										<NavLink to="/list" className="Nav-link">
+											List
+										</NavLink>
+										<NavLink to="/add-item" className="Nav-link">
+											Add Item
+										</NavLink>
+
+										{/* <Link underline="hover" color="inherit" href="/">
 										Home
 									</Link>
-									<Link
-										underline="hover"
-										color="inherit"
-										href="/material-ui/getting-started/installation/"
-									>
+									<Link underline="hover" color="inherit" href="/list">
 										List
 									</Link>
-									<Typography color="text.primary">Add Item</Typography>
-								</Breadcrumbs>
-							</div>
-						</Toolbar>
-					</Container>
-				</AppBar>
+									<Link underline="hover" color="inherit" href="/add-item">
+										Add Item
+									</Link> */}
+									</Breadcrumbs>
+								</div>
+
+								<Typography
+									variant="h6"
+									noWrap
+									component="a"
+									href=""
+									sx={{
+										mr: 2,
+										display: { xs: 'flex', md: 'none' },
+										justifyContent: 'right',
+										flexGrow: 1,
+										fontFamily: 'monospace',
+										fontWeight: 700,
+										letterSpacing: '.3rem',
+										color: 'inherit',
+										textDecoration: 'none',
+									}}
+								>
+									Smart Shopping List
+								</Typography>
+							</Toolbar>
+						</Container>
+					</AppBar>
+				</Container>
 
 				{/* <Container
 					maxWidth="lg"
 					sx={{ bgcolor: '#cfe8fc', display: 'flex', justifyContent: 'left' }}
 				>
+				
 					<header className="Layout-header">
-						<nav className="Nav">
+						
+						<Breadcrumbs aria-label="breadcrumb">
 							<NavLink to="/" className="Nav-link">
 								Home
 							</NavLink>
@@ -77,13 +113,14 @@ export function Layout() {
 							<NavLink to="/add-item" className="Nav-link">
 								Add Item
 							</NavLink>
-						</nav>
+						</Breadcrumbs>
+						
 						<Typography variant="h3" gutterBottom>
 							Smart shopping list
 						</Typography>
-					</header> */}
-				{/* </Container> */}
-				{/* <Paper> */}
+					</header> 
+				
+				</Container> */}
 
 				<Container
 					maxWidth="lg"
@@ -99,8 +136,6 @@ export function Layout() {
 						<Outlet />
 					</main>
 				</Container>
-
-				{/* </Paper> */}
 			</Container>
 		</>
 	);
