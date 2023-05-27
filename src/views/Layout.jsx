e9a08906f3c8aa96d4c5dfdf016d4e881bb8e5fe
amy_pr <ampreci@gmail.com>
2023-05-27 00:49:50 -0700

    rearrange order of elements in navbar and remove small-screenheadline Typography

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -13,6 +13,7 @@ import {
 	MenuItem,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import BreakfastDiningOutlinedIcon from '@mui/icons-material/BreakfastDiningOutlined';
 import './Layout.css';
 
 export function Layout({ listToken }) {
@@ -41,16 +42,70 @@ export function Layout({ listToken }) {
 				<Container maxWidth="xl" sx={{ p: '0' }} disableGutters>
 					<AppBar position="relative">
 						<Container maxWidth="xl">
-							<Toolbar sx={{ justifyContent: 'space-between' }}>
+							{/* <Toolbar sx={{ justifyContent: 'space-between' }}> */}
+							<Toolbar>
+								{/* This is the App Icon Menu */}
+								{listToken ? (
+									<Box
+										sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+									>
+										<IconButton
+											size="large"
+											aria-label="account of current user"
+											aria-controls="menu-appbar"
+											aria-haspopup="true"
+											onClick={handleOpenNavMenu}
+											color="inherit"
+										>
+											<MenuIcon />
+										</IconButton>
+
+										{/* This is the menu displayed after clicking on the icon */}
+										<Menu
+											id="menu-appbar"
+											anchorEl={anchorElNav}
+											anchorOrigin={{
+												vertical: 'bottom',
+												horizontal: 'left',
+											}}
+											keepMounted
+											transformOrigin={{
+												vertical: 'top',
+												horizontal: 'left',
+											}}
+											open={Boolean(anchorElNav)}
+											onClose={handleCloseNavMenu}
+											sx={{
+												display: { xs: 'block', md: 'none' },
+											}}
+										>
+											<MenuItem onClick={handleCloseNavMenu}>
+												<NavLink to="/list" className="Menu-nav-link">
+													List
+												</NavLink>
+											</MenuItem>
+
+											<MenuItem onClick={handleCloseNavMenu}>
+												<NavLink to="/add-item" className="Menu-nav-link">
+													Add Item
+												</NavLink>
+											</MenuItem>
+										</Menu>
+									</Box>
+								) : (
+									<Box></Box>
+								)}
+
 								{/* This is the title displayed on the left for medium to large screen sizes */}
+								<BreakfastDiningOutlinedIcon sx={{ mr: 1 }} />
 								<Typography
 									variant="h3"
-									noWrap
+									// noWrap
 									component="a"
 									href="/"
 									sx={{
 										mr: 2,
-										display: { xs: 'none', md: 'flex' },
+										flexGrow: 1,
 										fontFamily: 'monospace',
 										fontWeight: 700,
 										letterSpacing: '.3rem',
@@ -61,58 +116,26 @@ export function Layout({ listToken }) {
 									Smart Shopping List
 								</Typography>
 
-								{/* This is the App Icon Menu */}
-								{listToken ? (
-									<div>
-										<Box
-											sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-										>
-											<IconButton
-												size="large"
-												aria-label="account of current user"
-												aria-controls="menu-appbar"
-												aria-haspopup="true"
-												onClick={handleOpenNavMenu}
-												color="inherit"
-											>
-												<MenuIcon />
-											</IconButton>
-											{/* This is the menu displayed after clicking on the icon */}
-											<Menu
-												id="menu-appbar"
-												anchorEl={anchorElNav}
-												anchorOrigin={{
-													vertical: 'bottom',
-													horizontal: 'left',
-												}}
-												keepMounted
-												transformOrigin={{
-													vertical: 'top',
-													horizontal: 'left',
-												}}
-												open={Boolean(anchorElNav)}
-												onClose={handleCloseNavMenu}
-												sx={{
-													display: { xs: 'block', md: 'none' },
-												}}
-											>
-												<MenuItem onClick={handleCloseNavMenu}>
-													<NavLink to="/list" className="Menu-nav-link">
-														List
-													</NavLink>
-												</MenuItem>
-
-												<MenuItem onClick={handleCloseNavMenu}>
-													<NavLink to="/add-item" className="Menu-nav-link">
-														Add Item
-													</NavLink>
-												</MenuItem>
-											</Menu>
-										</Box>
-									</div>
-								) : (
-									<div></div>
-								)}
+								{/* This is the title displayed on the right for mobile version */}
+								{/* <BreakfastDiningOutlinedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1}}/>
+								<Typography
+									variant="h4"
+									// noWrap
+									component="a"
+									sx={{
+										mr: 2,
+										display: { xs: 'flex', md: 'none' },
+										// justifyContent: 'center',
+										flexGrow: 0,
+										fontFamily: 'monospace',
+										fontWeight: 700,
+										letterSpacing: '.3rem',
+										color: 'inherit',
+										textDecoration: 'none',
+									}}
+								>
+									Smart Shopping List
+								</Typography> */}
 
 								{/* These are the Nav Links that display on right side for medium and large screens*/}
 								{listToken ? (
@@ -134,26 +157,6 @@ export function Layout({ listToken }) {
 								) : (
 									<div></div>
 								)}
-
-								{/* This is the title displayed on the right for mobile version */}
-								<Typography
-									variant="h4"
-									// noWrap
-									component="a"
-									sx={{
-										mr: 2,
-										display: { xs: 'flex', md: 'none' },
-										justifyContent: 'right',
-										flexGrow: 1,
-										fontFamily: 'monospace',
-										fontWeight: 700,
-										letterSpacing: '.3rem',
-										color: 'inherit',
-										textDecoration: 'none',
-									}}
-								>
-									Smart Shopping List
-								</Typography>
 							</Toolbar>
 						</Container>
 					</AppBar>
