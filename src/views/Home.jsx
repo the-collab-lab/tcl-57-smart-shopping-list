ae9cf975ce439b1a03c4429fb351a4bcf87b8da6
Jon Granados <grxnxdos@gmail.com>
2023-05-27 21:01:25 -0700

    style: responsive logo display on mobile

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { Navigate } from 'react-router-dom';
-import { Box, Button, TextField, Typography, Stack } from '@mui/material';
+import {
+	Box,
+	Button,
+	TextField,
+	Typography,
+	Stack,
+	useMediaQuery,
+} from '@mui/material';
 
 import logoFlat from '../logo-flat.png';
 import logoShadow from '../logo-shadow.png';
@@ -14,6 +21,7 @@ import 'toastify-js/src/toastify.css';
 export function Home({ setListToken }) {
 	const [userTokenInput, setUserTokenInput] = useState('');
 	const [tokenExists, setTokenExists] = useState(false);
+	const matchesMobileDevice = useMediaQuery('(max-width:600px)');
 
 	function handleClick() {
 		const token = generateToken();
@@ -39,7 +47,11 @@ export function Home({ setListToken }) {
 
 	return (
 		<Stack direction="column" justifyContent="center" alignItems="center">
-			<img src={logoFlat} alt="logo" height="300" width="300" />
+			<img
+				src={logoFlat}
+				alt="logo"
+				width={matchesMobileDevice ? '200' : '300'}
+			/>
 
 			<Stack gap={2} alignItems="center">
 				<Typography variant="h1" marginBottom={1}>
